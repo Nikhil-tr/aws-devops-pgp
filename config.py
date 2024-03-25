707611006ae9f2eeff25e3d1da0fe7870adaf708
Nikhil-tr <152260396+Nikhil-tr@users.noreply.github.com>
2024-03-25 18:57:10 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
-custombucket = "destinationtxt"
+custombucket = "projs3-494273343511"
 table = "intellipaat"
-databasehost = "database-1.c14bnmt8jlz0.us-east-1.rds.amazonaws.com"
+databasehost = "database-1.c1a8umimc7ma.us-east-1.rds.amazonaws.com"
 duser = "intel"
 dpass = "intel123"
 s3database = "intellipaat"
